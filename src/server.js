45fa967bc59d273b10f090edb1c0c691d5db5ddf
Eugene <eugene97@connect.hku.hk>
2021-06-13 20:19:56 +0800

    first commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const http = require('http')
+
+const app = require('./app');
+
+const { loadPlanetData } = require('./models/planets.model')
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app)
+
+async function startServer() {
+    await loadPlanetData();
+
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`)
+    })
+}
+
+startServer();
