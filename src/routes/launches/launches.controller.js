dead8f1e9d3d66f690071077c720f3cac0a5e549
Eugene <eugene97@connect.hku.hk>
2021-06-13 22:06:12 +0800

    add test cases

--- src/routes/launches/launches.controller.js
+++ src/routes/launches/launches.controller.js
@@ -17,7 +17,7 @@ function httpAddNewLaunch(req, res) {
     launch.launchDate = new Date(launch.launchDate)
     if (isNaN(launch.launchDate)){
         return res.status(400).json({
-            error: 'Invalid launch Date'
+            error: 'Invalid launch date'
         })
     }
 
