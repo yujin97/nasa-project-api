b5cfb3e58a994f8a8e29bdb1df34b2abb440dd71
Eugene <eugene97@connect.hku.hk>
2021-06-27 11:38:48 +0800

    add models

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,9 +7,25 @@ const { loadPlanetData } = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = "mongodb+srv://nasa-api:fpzXfz3JrTeWBD3H@nasacluster.hkwxb.mongodb.net/nasa?retryWrites=true&w=majority"
+
 const server = http.createServer(app)
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error(err)
+})
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
     await loadPlanetData();
 
     server.listen(PORT, () => {
